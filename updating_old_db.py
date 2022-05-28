e3d7c95ed97685c9d59f481162bc670b44a2e0b3
JonathanQueg <joangelapython@gmail.com>
2022-05-28 17:21:36 +0300

    Updating Old Post DB

---
+++ updating_old_db.py
@@ -0,0 +1,19 @@
+import json
+from main import db, BlogPost
+from datetime import date
+
+with open('blog.json', "r") as data:
+    # Reading old data
+    data_read = json.load(data)
+
+for i in data_read:
+    new_post = BlogPost(
+        title=data_read[i]['Title'],
+        subtitle=data_read[i]['Subtitle'],
+        body=data_read[i]['Body'],
+        img_url=data_read[i]['img_url'],
+        author=1,
+        date=date.today().strftime("%B %d, %Y")
+    )
+    db.session.add(new_post)
+    db.session.commit()
